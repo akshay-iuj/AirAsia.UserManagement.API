817926ccac9546c1469a19c5823f5f2e9445fa21
akshay-iuj <akshay.iuj@gmail.com>
2021-01-25 13:28:15 +0530

    Update LoginRequest.java

--- src/main/java/com/airasia/usermanagement/payload/request/LoginRequest.java
+++ src/main/java/com/airasia/usermanagement/payload/request/LoginRequest.java
@@ -5,17 +5,17 @@ import org.hibernate.validator.constraints.NotBlank;
 
 public class LoginRequest {
     @NotBlank
-    private String usernameoremail;
+    private String username;
 
     @NotBlank
     private String password;
 
-    public String getUsernameOrEmail() {
-        return usernameoremail;
+    public String getUsername() {
+        return username;
     }
 
     public void setUsernameOrEmail(String username) {
-        this.usernameoremail = username;
+        this.username = username;
     }
 
     public String getPassword() {
